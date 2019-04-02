f7d2738e579f11a08a79768a9d8e9a67eeecab86
Roku250 <40688526+Roku250@users.noreply.github.com>
2019-04-02 20:57:08 +0200

    fonction supprimer

---
+++ Microlibrairie/Microlibrairie/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Microlibrairie
+{
+    public partial class Microlibrairie : Form
+    {
+        string pass = "";
+        string connectionString = @"Server=localhost;Database=bookdb;Uid=root;Pwd=;";
+        int id;
+        
+
+        public Microlibrairie()
+        {
+            InitializeComponent();
+        }
+
+        private void Microlibrairie_Load(object sender, EventArgs e)
+        {
+            GridFill();
+            Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("BookAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_id",id);
+                mySqlCmd.Parameters.AddWithValue("_nom", textBox1.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_auteur", textBox2.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_description", textBox3.Text.Trim());
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("C'est bien ajouté");
+                Clear();
+                GridFill();
+            }
+        }
+        void GridFill()
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("BookViewAll", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtblLivre = new DataTable();
+                sqlDa.Fill(dtblLivre);
+                dataGridView1.DataSource = dtblLivre;
+                dataGridView1.Columns[0].Visible = false;
+            }
+        }
+        void Clear()
+        {
+            textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text = "";
+            id = 0;
+            button1.Text = "Ajouter";
+            button2.Enabled = false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if(dataGridView1.CurrentRow.Index != 1)
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+                button1.Text = "Update";
+                button2.Enabled = Enabled;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("BookSearchByValue", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("_SearchValue", textBox4.Text);
+                DataTable dtblLivre = new DataTable();
+                sqlDa.Fill(dtblLivre);
+                dataGridView1.DataSource = dtblLivre;
+                dataGridView1.Columns[0].Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("BookDeleteID", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_id", id);
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Livre supprimé");
+                Clear();
+                GridFill();
+            }
+        }
+    }
+}
